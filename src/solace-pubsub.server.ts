97ffbb3c54b946bdf359658087b02049aef43713
Chun-Kai Wang <chunkai1312@gmail.com>
2023-10-28 16:55:30 +0800

    Revert "refactor: The client sends using Solace reply functionality (#1)"
    This reverts commit 27e9f047d818d8c8813e0dea8edef0ab44e50188.

--- src/solace-pubsub.server.ts
+++ src/solace-pubsub.server.ts
@@ -1,33 +1,20 @@
-import {
-  Message,
-  Session,
-  SessionEventCode,
-  SolclientFactory,
-} from 'solclientjs';
-import {
-  CustomTransportStrategy,
-  PacketId,
-  ReadPacket,
-  Server,
-} from '@nestjs/microservices';
+import { Message, MessageDeliveryModeType, Session, SessionEventCode, SolclientFactory } from 'solclientjs';
+import { isUndefined } from '@nestjs/common/utils/shared.utils';
+import { NO_MESSAGE_HANDLER } from '@nestjs/microservices/constants';
+import { CustomTransportStrategy, IncomingRequest, MessageHandler, PacketId, ReadPacket, Server } from '@nestjs/microservices';
 import { SolacePubSubOptions } from './solace-pubsub-options.interface';
 import { SolacePubSubContext } from './solace-pubsub.context';
 
 let solacePackage: any = {};
 
-export class SolacePubSubServer
-  extends Server
-  implements CustomTransportStrategy
-{
+export class SolacePubSubServer extends Server implements CustomTransportStrategy {
   protected session: Session;
 
   constructor(private readonly options: SolacePubSubOptions) {
     super();
 
-    solacePackage = this.loadPackage(
-      'solclientjs',
-      SolacePubSubServer.name,
-      () => require('solclientjs'),
+    solacePackage = this.loadPackage('solclientjs', SolacePubSubServer.name, () =>
+      require('solclientjs'),
     );
     const factoryProps = new solacePackage.SolclientFactoryProperties();
     factoryProps.profile = solacePackage.SolclientFactoryProfiles.version10;
@@ -60,16 +47,11 @@ export class SolacePubSubServer
   }
 
   public bindEvents(session: Session) {
-    session.on(
-      SessionEventCode.MESSAGE,
-      this.getMessageHandler(session).bind(this),
-    );
+    session.on(SessionEventCode.MESSAGE, this.getMessageHandler(session).bind(this));
     const registeredPatterns = [...this.messageHandlers.keys()];
-    registeredPatterns.forEach((pattern) => {
+    registeredPatterns.forEach(pattern => {
       const { isEventHandler } = this.messageHandlers.get(pattern);
-      const topicName = isEventHandler
-        ? pattern
-        : this.getRequestPattern(pattern);
+      const topicName = isEventHandler ? pattern : this.getRequestPattern(pattern);
       this.session.subscribe(
         SolclientFactory.createTopicDestination(topicName),
         true,
@@ -89,26 +71,57 @@ export class SolacePubSubServer
     return session;
   }
 
-  public getMessageHandler(pub: Session) {
-    return async (message: Message) => this.handleMessage(message, pub);
+  public getMessageHandler(pub: Session): Function {
+    return async (
+      message: Message,
+    ) => this.handleMessage(message, pub);
   }
 
-  public async handleMessage(message: Message, replier: Session): Promise<any> {
-    // console.log(message);
+  public async handleMessage(
+    message: Message,
+    pub: Session,
+  ): Promise<any> {
     const pattern = message.getDestination().name;
     const rawPacket = this.parseMessage(message.getBinaryAttachment());
     const packet = await this.deserializer.deserialize(rawPacket);
     const context = new SolacePubSubContext([message]);
-
+    if (isUndefined((packet as IncomingRequest).id)) {
+      return this.handleEvent(pattern, packet, context);
+    }
+    const publish = this.getPublisher(
+      pub,
+      pattern,
+      (packet as IncomingRequest).id,
+    );
     const handler = this.getHandlerByPattern(pattern);
 
-    const result = await handler(packet.data, context);
-
-    if (message.getReplyTo()) {
-      const reply = SolclientFactory.createMessage();
-      reply.setBinaryAttachment(JSON.stringify({ ...packet, data: result }));
-      replier.sendReply(message, reply);
+    if (!handler) {
+      const status = 'error';
+      const noHandlerPacket = {
+        id: (packet as IncomingRequest).id,
+        status,
+        err: NO_MESSAGE_HANDLER,
+      };
+      return publish(noHandlerPacket);
     }
+    const response$ = this.transformToObservable(
+      await handler(packet.data, context),
+    );
+    response$ && this.send(response$, publish);
+  }
+
+  public getPublisher(session: Session, pattern: any, id: string): any {
+    return (response: any) => {
+      Object.assign(response, { id });
+      const outgoingResponse = this.serializer.serialize(response);
+
+      const message = SolclientFactory.createMessage();
+      message.setDestination(SolclientFactory.createTopicDestination(this.getReplyPattern(pattern)));
+      message.setBinaryAttachment(JSON.stringify(outgoingResponse));
+      message.setDeliveryMode(MessageDeliveryModeType.DIRECT);
+
+      return session.send(message);
+    };
   }
 
   public parseMessage(content: any): ReadPacket & PacketId {
@@ -123,9 +136,11 @@ export class SolacePubSubServer
     return pattern;
   }
 
+  public getReplyPattern(pattern: string): string {
+    return `${pattern}/reply`;
+  }
+
   public handleError(session: any) {
-    session.on(SessionEventCode.DOWN_ERROR, (err: any) =>
-      this.logger.error(err),
-    );
+    session.on(SessionEventCode.DOWN_ERROR, (err: any) => this.logger.error(err));
   }
 }
